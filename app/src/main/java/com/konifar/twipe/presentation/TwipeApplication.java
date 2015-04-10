f396ce303ba4f8d29d2089623eb52e149f8054bd
konifar <yahpeycoy0403@gmail.com>
2015-04-11 04:16:36 +0900

    Create themes and styles

--- app/src/main/java/com/konifar/twipe/presentation/TwipeApplication.java
+++
@@ -1,8 +0,0 @@
-package com.konifar.twipe.presentation;
-
-import android.app.Application;
-
-public class TwipeApplication extends Application {
-
-  private static String TAG = TwipeApplication.class.getSimpleName();
-}
