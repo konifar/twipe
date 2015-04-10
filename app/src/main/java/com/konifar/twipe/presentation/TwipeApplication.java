6112806e7230d5daf3a965b008977486dd2505af
konifar <yahpeycoy0403@gmail.com>
2015-04-11 03:52:54 +0900

    Create android project

---
+++ app/src/main/java/com/konifar/twipe/presentation/TwipeApplication.java
@@ -0,0 +1,8 @@
+package com.konifar.twipe.presentation;
+
+import android.app.Application;
+
+public class TwipeApplication extends Application {
+
+  private static String TAG = TwipeApplication.class.getSimpleName();
+}
