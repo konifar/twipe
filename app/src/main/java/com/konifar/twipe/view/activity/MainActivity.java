17b7838604a0580e9ea1ca27369507808aab509d
konifar <yahpeycoy0403@gmail.com>
2015-06-30 00:24:50 +0900

    Implements login logic

--- app/src/main/java/com/konifar/twipe/view/activity/MainActivity.java
+++ app/src/main/java/com/konifar/twipe/view/activity/MainActivity.java
@@ -1,13 +1,37 @@
 package com.konifar.twipe.view.activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import com.konifar.twipe.R;
+import com.konifar.twipe.view.fragment.TweetListFragment;
+import com.twitter.sdk.android.Twitter;
 
 public class MainActivity extends BaseActivity {
+
+  @InjectView(R.id.toolbar) Toolbar toolbar;
+
+  public static void start(Context context) {
+    Intent intent = new Intent(context, MainActivity.class);
+    context.startActivity(intent);
+  }
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    LoginActivity.start(this);
+    ButterKnife.inject(this);
+    setSupportActionBar(toolbar);
+
+    if (Twitter.getSessionManager().getActiveSession() == null) {
+      LoginActivity.start(this);
+      finish();
+    } else {
+      if (savedInstanceState == null) {
+        addFragment(R.id.container, new TweetListFragment());
+      }
+    }
   }
 }
