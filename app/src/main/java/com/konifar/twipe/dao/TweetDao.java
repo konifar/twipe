8963e1ef13f16afb515576a9dbf8bba70cc4a02b
konifar <yahpeycoy0403@gmail.com>
2015-07-01 02:07:59 +0900

    Add tab

--- app/src/main/java/com/konifar/twipe/dao/TweetDao.java
+++ app/src/main/java/com/konifar/twipe/dao/TweetDao.java
@@ -4,23 +4,27 @@ import com.konifar.twipe.model.pojo.TweetModel;
 import java.util.Collection;
 
 public interface TweetDao {
-  void getHomeTweetList(final Long lastTweetId);
+  void getHomeTweetList(final Long lastTweetId, final String tag);
 
-  void getUserTweetList(final long userId, final Long lastTweetId);
+  void getUserTweetList(final long userId, final Long lastTweetId, final String tag);
 
   public class OnLoadedEvent {
     public final Collection<TweetModel> tweetModels;
+    public final String tag;
 
-    public OnLoadedEvent(Collection<TweetModel> tweetModels) {
+    public OnLoadedEvent(Collection<TweetModel> tweetModels, String tag) {
       this.tweetModels = tweetModels;
+      this.tag = tag;
     }
   }
 
   public class OnErrorEvent {
     public final Exception exception;
+    public final String tag;
 
-    public OnErrorEvent(Exception exception) {
+    public OnErrorEvent(Exception exception, String tag) {
       this.exception = exception;
+      this.tag = tag;
     }
   }
 }
---
+++ app/src/main/java/com/konifar/twipe/view/adapter/TweetListPagerAdappter.java
@@ -0,0 +1,34 @@
+package com.konifar.twipe.view.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TweetListPagerAdappter extends FragmentPagerAdapter {
+
+  private final List<Fragment> fragments = new ArrayList<>();
+  private final List<String> fragmentTitles = new ArrayList<>();
+
+  public TweetListPagerAdappter(FragmentManager fm) {
+    super(fm);
+  }
+
+  public void addFragment(Fragment fragment, String title) {
+    fragments.add(fragment);
+    fragmentTitles.add(title);
+  }
+
+  @Override public Fragment getItem(int position) {
+    return fragments.get(position);
+  }
+
+  @Override public int getCount() {
+    return fragments.size();
+  }
+
+  @Override public CharSequence getPageTitle(int position) {
+    return fragmentTitles.get(position);
+  }
+}
